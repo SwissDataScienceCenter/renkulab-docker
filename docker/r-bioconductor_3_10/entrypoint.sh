0b7e84586c5b8d06c85a868a5eacbda05c62b637
Emma Jablonski <e.jablonski@gmail.com>
2020-02-10 10:07:02 +0100

    adding the entrypoint project opening to r-bioc also

--- docker/r-bioconductor_3_10/entrypoint.sh
+++ docker/r-bioconductor_3_10/entrypoint.sh
@@ -19,6 +19,28 @@ fi
 # add a symlink to the project directory in /home/rstudio
 [ -n "$CI_PROJECT" ] && ln -s /work/${CI_PROJECT} /home/rstudio
 
+# configure rstudio to open the rpath project
+if [[ ! -f /home/rstudio/${CI_PROJECT}/${CI_PROJECT}.Rproj ]]; then
+cat > /home/rstudio/${CI_PROJECT}/${CI_PROJECT}.Rproj <<- EOM
+Version: 1.0
+
+RestoreWorkspace: Default
+SaveWorkspace: Default
+AlwaysSaveHistory: Default
+
+EnableCodeIndexing: Yes
+UseSpacesForTab: Yes
+NumSpacesForTab: 2
+Encoding: UTF-8
+
+RnwWeave: Sweave
+LaTeX: pdfLaTeX
+EOM
+fi
+mkdir -p /home/rstudio/.rstudio/projects_settings
+echo /home/rstudio/${CI_PROJECT}/${CI_PROJECT}.Rproj | tee /home/rstudio/.rstudio/projects_settings/next-session-project
+chown -R rstudio:root /home/rstudio/.rstudio/projects_settings
+
 #
 # copy the environment from renku-env repo
 #
