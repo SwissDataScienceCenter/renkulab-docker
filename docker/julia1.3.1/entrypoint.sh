031fa698444a460ca9d7acae03a44cd37b5e5db2
Rok Roškar <roskarr@ethz.ch>
2020-04-03 15:33:46 +0200

    feat: add julia (#84)
    Co-authored-by: Emma Jablonski <e.jablonski@gmail.com>

---
+++ docker/julia1.3.1/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Setup git user
+if [ -z "$(git config --global --get user.name)" ]; then
+    git config --global user.name "$GIT_AUTHOR_NAME"
+fi
+if [ -z "$(git config --global --get user.email)" ]; then
+    git config --global user.email "$EMAIL"
+fi
+
+#
+# copy the environment from renku-env repo
+#
+
+# clone the repo
+proto=$(echo $GITLAB_URL | sed -e's,^\(.*://\).*,\1,g')
+url=$(echo ${GITLAB_URL/$proto/})
+user=$(echo ${CI_REPOSITORY_URL/$proto/} | grep @ | cut -d@ -f1)
+
+git clone --depth 1 ${proto}${user}@${url}/${JUPYTERHUB_USER}/renku-env.git /tmp/renku-env || true
+
+# append the contents of all the files to same files in ${HOME}
+find /tmp/renku-env -not -path '*.git*' -type f -print0 | xargs --null -I{} sh -c 'cat {} >> ${HOME}/$(basename "{}")' || true
+
+# run the command
+$@
