c50af197ccd7b727b9baa0aebf60602c669ab546
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2020-11-25 15:09:12 +0100

    build: move vscode install to a script

---
+++ scripts/install-vscode.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+VSCODE_VERSION=${VSCODE_VERSION:="3.6.2"}
+
+# code-server installation
+wget https://github.com/cdr/code-server/releases/download/v${VSCODE_VERSION}/code-server_3.6.2_amd64.deb
+dpkg -i ./code-server*.deb
+rm code-server_3.6.2_amd64.deb
+apt-get clean
+code-server --install-extension ms-python.python
+chown -R 1000:1000 /home/${NB_USER}/.local/share
+
+# Jupyter support
+pip install git+https://github.com/betatim/vscode-binder
+jupyter serverextension enable --py jupyter_server_proxy && \
+jupyter labextension install --no-build @jupyterlab/server-proxy
