7ac7b09a51245d9e1c79da3a4c7571094f05f9ca
Rok Roškar <roskarr@ethz.ch>
2021-02-10 17:19:01 +0100

    Update install-vscode.sh

--- scripts/install-vscode.sh
+++ scripts/install-vscode.sh
@@ -5,7 +5,7 @@ VSCODE_VERSION=${VSCODE_VERSION:="3.8.1"}
 # code-server installation
 wget https://github.com/cdr/code-server/releases/download/v${VSCODE_VERSION}/code-server_${VSCODE_VERSION}_amd64.deb
 dpkg -i ./code-server*.deb
-rm code-server_3.6.2_amd64.deb
+rm code-server_${VSCODE_VERSION}_amd64.deb
 apt-get clean
 code-server --install-extension ms-python.python
 chown -R 1000:1000 /home/${NB_USER}/.local/share
