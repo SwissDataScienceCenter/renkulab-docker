0fcc8a05885153939d47302a4c76e67149a20a10
Rok Roškar <roskarr@ethz.ch>
2021-02-11 01:08:05 +0100

    fix the broken python extension

--- scripts/install-vscode.sh
+++ scripts/install-vscode.sh
@@ -7,10 +7,17 @@ wget https://github.com/cdr/code-server/releases/download/v${VSCODE_VERSION}/cod
 dpkg -i ./code-server*.deb
 rm code-server_${VSCODE_VERSION}_amd64.deb
 apt-get clean
-code-server --install-extension ms-python.python
+
+# Fix broken python plugin # https://github.com/cdr/code-server/issues/2341
+mkdir -p /home/{$NB_USER}/.local/share/code-server/
+mkdir -p /home/{$NB_USER}/.local/share/code-server/User
+echo "{\"extensions.autoCheckUpdates\": false, \"extensions.autoUpdate\": false}" > /home/{$NB_USER}/.local/share/code-server/User/settings.json
+wget https://github.com/microsoft/vscode-python/releases/download/2020.10.332292344/ms-python-release.vsix
+code-server --install-extension ./ms-python-release.vsix || true
+rm -rf ./ms-python-release.vsix
 chown -R 1000:1000 /home/${NB_USER}/.local/share
 
 # Jupyter support
 pip install git+https://github.com/betatim/vscode-binder
-jupyter serverextension enable --py jupyter_server_proxy && \
+jupyter serverextension enable --py jupyter_server_proxy
 jupyter labextension install @jupyterlab/server-proxy
