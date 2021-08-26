1dd8154b2c1c066e9aa37691ea6f03ae3e504300
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-08-26 20:59:35 +0200

    feat: make images fully amalthea ready
    The goal of this is to move maintain as much of the current bahaviour
    with Amalthea-based sessions, while also keeping the images backwards
    compatible for now.

--- docker/julia/post-init.sh
+++ docker/julia/post-init.sh
@@ -2,6 +2,8 @@
 
 # Slip in a replacement for the julia command that sets uses the project
 # environment rather than the root environment
+
+# Pre amalthea
 if [[ -v CI_PROJECT ]];
 then
     ln -sf /work/${CI_PROJECT} ~/work
@@ -9,3 +11,12 @@ then
     printf '/usr/local/bin/julia --project=/work/%s/ "$@"\n' ${CI_PROJECT} >> ~/.local/bin/julia
     chmod u+x ~/.local/bin/julia
 fi
+
+# With amalthea
+if [[ -v PROJECT_NAME && -v NOTEBOOK_DIR ]];
+then
+    echo '#!/usr/bin/env bash' > ~/.local/bin/julia
+    printf '/usr/local/bin/julia --project=%s/ "$@"\n' ${NOTEBOOK_DIR} >> ~/.local/bin/julia
+    chmod u+x ~/.local/bin/julia
+fi
+
--- docker/py/entrypoint.sh
+++ docker/py/entrypoint.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-# Setup git user
-if [ -z "$(git config --global --get user.name)" ]; then
-    git config --global user.name "$GIT_AUTHOR_NAME"
-fi
-if [ -z "$(git config --global --get user.email)" ]; then
-    git config --global user.email "$EMAIL"
-fi
-
-# configure global git credentials
-if [[ -v CI_PROJECT ]];
+if [[ -v GIT_AUTHOR_NAME && -v EMAIL && -v CI_PROJECT ]];
 then
-    # set the global git credentials
-    git config --global credential.helper "store --file=/work/${CI_PROJECT}/.git/credentials"
+    # Setup git user
+    if [ -z "$(git config --global --get user.name)" ]; then
+        git config --global user.name "$GIT_AUTHOR_NAME"
+    fi
+    if [ -z "$(git config --global --get user.email)" ]; then
+        git config --global user.email "$EMAIL"
+    fi
 
-    # link to the home work directory
-    ln -sf /work/${CI_PROJECT} ~/work
+    # configure global git credentials
+    if [[ -v CI_PROJECT ]];
+    then
+        # set the global git credentials
+        git config --global credential.helper "store --file=/work/${CI_PROJECT}/.git/credentials"
+
+        # link to the home work directory
+        ln -sf /work/${CI_PROJECT} ~/work
+    fi
 fi
-
-#
-# copy the environment from renku-env repo
-#
-
-# clone the repo
-proto=$(echo $GITLAB_URL | sed -e's,^\(.*://\).*,\1,g')
-url=$(echo ${GITLAB_URL/$proto/})
-user=$(echo ${CI_REPOSITORY_URL/$proto/} | grep @ | cut -d@ -f1)
-
-git clone --depth 1 ${GITLAB_URL}/${JUPYTERHUB_USER}/renku-env.git /tmp/renku-env || true
-
-# append the contents of all the files to same files in ${HOME}
-find /tmp/renku-env -not -path '*.git*' -type f -print0 | xargs --null -I{} sh -c 'cat {} >> ${HOME}/$(basename "{}")' || true
 
 # install git hooks
 ~/.local/bin/renku githooks install || true
