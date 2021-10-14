70fc724f35b6dbae0bc028af7850c4f182235b18
Gavin Lee <lee.gavin.k@gmail.com>
2021-10-14 14:06:06 +0200

    add dlc for GHA

--- docker/vnc/post-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Make the default terminal folder the project folder in the vnc
-echo "cd /work/${CI_PROJECT}" >> ~/.bashrc
-# Turn the bell off
-echo "bind 'set bell-style none'" >> ~/.bashrc
