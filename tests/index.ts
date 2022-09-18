d56230b5fcfd21648de53d410881b7c64ac781ea
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2022-09-18 15:37:58 +0200

    chore(ci): common tests library and rstudio tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,36 +1,47 @@
-var assert = require('assert');
 const util = require('util');
+const assert_ = require('assert');
 const exec = util.promisify(require('child_process').exec);
 const axios = require('axios').default;
+const wrapper = require("axios-cookiejar-support").wrapper
+const CookieJar = require("tough-cookie").CookieJar
+const cypress = require('cypress');
 
+const jar = new CookieJar();
+const client = wrapper(axios.create({ jar }));
 const imageName = process.env.TEST_IMAGE_NAME
 const port = process.env.PORT || 8888
-const url = process.env.URL || `http://127.0.0.1:${port}/lab`
+const env_name = process.env.TEST_ENV || "lab"
+const url = process.env.URL || `http://127.0.0.1:${port}/${env_name}`
 const containerName = "testing-dockerlab-image"
 const user = process.env.TEST_USER_NAME || "jovyan"
+const timeoutSeconds = process.env.TIMEOUT_SECS as unknown as number || 600;
+const testSpec = process.env.TEST_SPEC || "jupyterlab.cy.ts";
 
-function sleep(ms) {
+function sleep(ms: number) {
   // shamelessly ripped off from https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const checkStatusCode = async function (url) {
+const checkStatusCode = async function (url: string) {
   var count = 0;
-  const maxCount = 10;
   while (true) {
     console.log("Waiting for container to become ready...")
     try {
-      res = await axios.get(url)
+      const res = await client.get(url)
       if (res.status < 300) {
+        console.log(`Response from starting container succeeded with status code: ${res.status}`)
         return {"status": res.status};
       }
     }
-    catch (err) {}
+    catch (err) {
+      console.log(`Waiting to start for a container failed with error: ${err}.`)
+    }
     finally {
-      if (count > maxCount) {
+      if (count > timeoutSeconds / 10) {
+        console.log("Waiting for container to become available timed out.")
         return {"error": "Timed out waiting for container to become ready"}
       }
-      await sleep(1000);
+      await sleep(10000);
       count = count + 1;
     }
   }
@@ -40,16 +51,19 @@ describe(`Starting container with image ${imageName} and name ${containerName}`,
   this.timeout(0);
   before(async function () {
     await exec(`docker run -d -p ${port}:${port} --name ${containerName} --rm --user ${user} --entrypoint="tini" ${imageName} "-g" "--" jupyter lab --ServerApp.port=${port} --NotebookApp.port=${port} --ServerApp.ip=0.0.0.0 --NotebookApp.ip=0.0.0.0 --ServerApp.token="" --NotebookApp.token="" --ServerApp.password=""  --NotebookApp.password="" --NotebookApp.disable_check_xsrf="true" --ServerApp.disable_check_xsrf="true"`);
-    const {_, error} = await checkStatusCode(url);
-    assert(!error)
+    const {error} = await checkStatusCode(url);
+    assert_(!error)
   });
   it('Should pass all acceptance tests', async function () {
-    const {stdout, stderr, error} = await exec(`npx cypress run --env URL=${url}`);
-    console.log(`\n\n--------------------------------------------Cypress stdout--------------------------------------------\n${stdout}`)
-    console.log(`\n\n--------------------------------------------Cypress stderr--------------------------------------------\n${stderr}`)
-    console.log(`\n\n--------------------------------------------Cypress error--------------------------------------------\n${error}`)
-    console.log(`\n\n-----------------------------------------------------------------------------------------------------\n`)
-    assert(!error)
+    const results = await cypress.run({
+      env: {
+        URL: url
+      },
+      spec: `cypress/e2e/${testSpec}`,
+      configFile: "cypress.config.ts",
+      browser: "chrome"
+    })
+    assert_(!results.totalFailed, `Tests failed with errors on chrome.`)
   });
   after(async function () {
     console.log(`Stopping container with image ${imageName} and name ${containerName}`)
