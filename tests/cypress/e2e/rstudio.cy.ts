2047521abdf2f1b2c5bddfb8dccd2319db8da8f0
Flora Thiebaut <johann.thiebaut@gmail.com>
2024-03-27 17:19:56 +0100

    fix tests (#444)
    fix acceptance test by using a fixed and unreleased version of the test package

--- tests/cypress/e2e/rstudio.cy.ts
+++ tests/cypress/e2e/rstudio.cy.ts
@@ -1,3 +1,46 @@
-import { basicRstudioTests } from "@renku/notebooks-cypress-tests";
+import {
+  rstudioTestFuncs,
+  registerCustomCommands,
+  validateLogin,
+} from "@renku/notebooks-cypress-tests";
 
-basicRstudioTests(Cypress.env("URL"));
+const username = Cypress.env("USERNAME");
+const password = Cypress.env("PASSWORD");
+const url_or_path = Cypress.env("URL");
+const url =
+  url_or_path.slice(-1) === "/" ? url_or_path.slice(0, -1) : url_or_path;
+
+describe("Basic Rstudio functionality", () => {
+  before(() => {
+    registerCustomCommands();
+  });
+  beforeEach(() => {
+    cy.session(
+      "login-rstudio",
+      () => {
+        cy.visit(url);
+        cy.renkuLoginIfRequired(username, password);
+      },
+      {
+        validate: () => validateLogin(username, password),
+      }
+    );
+    cy.visit(url);
+    cy.wait(100);
+  });
+  it("Successfully loads", rstudioTestFuncs.findExpectedElements);
+  it("Can launch a terminal", rstudioTestFuncs.launchTerminal);
+  it(
+    "Can run terminal command to create a file",
+    rstudioTestFuncs.makeFileWithTerminal("new-file.txt")
+  );
+  it(
+    "Can remove the file",
+    rstudioTestFuncs.removeFileWithTerminal("new-file.txt")
+  );
+  it("Can close the terminal", rstudioTestFuncs.closeTerminal);
+  it(
+    "Can find expected start page elements again",
+    rstudioTestFuncs.findExpectedElements
+  );
+});
