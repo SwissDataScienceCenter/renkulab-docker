b8e1f89846544650272880f3f5529de9a8738948
Rok Roškar <rok.roskar@sdsc.ethz.ch>
2024-08-26 14:51:36 +0200

    Revert "test the test fix"
    This reverts commit 1117afeebaab04d0acfcae7d093d1d2167d779cf.

--- tests/cypress/e2e/jupyterlab.cy.ts
+++ tests/cypress/e2e/jupyterlab.cy.ts
@@ -10,26 +10,6 @@ const url_or_path = Cypress.env("URL");
 const url =
   url_or_path.slice(-1) === "/" ? url_or_path.slice(0, -1) : url_or_path;
 
-const makeFileWithTerminal_ = (fname: string) => () => {
-  cy.get(".xterm-helper-textarea").click().type(`touch ${fname}{enter}`);
-  cy.get("#filebrowser > .jp-Toolbar")
-    .find('[title="Refresh the file browser."]')
-    .click();
-  cy.get("#filebrowser .jp-DirListing-content")
-    .should("be.visible")
-    .should("contain.text", "new-file.txt");
-};
-
-const removeFileWithTerminal_ = (fname: string) => () => {
-  cy.get(".xterm-helper-textarea").click().type(`rm ${fname}{enter}`);
-  cy.get("#filebrowser > .jp-Toolbar")
-    .find('[title="Refresh the file browser."]')
-    .click();
-  cy.get("#filebrowser .jp-DirListing-content")
-    .should("be.visible")
-    .should("not.contain.text", "new-file.txt");
-};
-
 describe("Test jupyter notebook", () => {
   before(() => {
     registerCustomCommands();
@@ -52,11 +32,11 @@ describe("Test jupyter notebook", () => {
   it("Can launch a terminal", jupyterlabTestFuncs.launchTerminal);
   it(
     "Can run terminal command to create a file",
-    makeFileWithTerminal_("new-file.txt")
+    jupyterlabTestFuncs.makeFileWithTerminal("new-file.txt")
   );
   it(
     "Can remove the file",
-    removeFileWithTerminal_("new-file.txt")
+    jupyterlabTestFuncs.removeFileWithTerminal("new-file.txt")
   );
   it("Can close the terminal", jupyterlabTestFuncs.closeTerminal);
   it(
