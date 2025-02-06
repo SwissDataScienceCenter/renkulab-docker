253211f04f1ce4e0db7128052873a6c3c5cc8758
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-02-06 14:57:58 +0100

    revert changes from main

--- docker/vscode/entrypoint.sh
+++ docker/vscode/entrypoint.sh
@@ -1,43 +1,12 @@
-#!/bin/bash
-
 set -ex
-
-HOME="${HOME:-/home/ubuntu}"
-RENKU_MOUNT_DIR="${RENKU_MOUNT_DIR:-${HOME}/work}"
-
-mkdir -p "${RENKU_MOUNT_DIR}/.vscode/extensions"
-
-# Install python extensions
-DEFAULT_VSCODIUM_EXTENSIONS="\
-  ms-python.python \
-  ms-toolsai.jupyter \
-"
-VSCODIUM_EXTENSIONS="${VSCODIUM_EXTENSIONS:-${DEFAULT_VSCODIUM_EXTENSIONS}}"
-VSCODIUM_EXTENSIONS=($VSCODIUM_EXTENSIONS)
-for ext in "${VSCODIUM_EXTENSIONS[@]}"; do
-  /codium-server/bin/codium-server \
-      --extensions-dir "${RENKU_MOUNT_DIR}/.vscode/extensions" \
-    --server-data-dir "${RENKU_MOUNT_DIR}/.vscode" \
-    --install-extension "${ext}" || true
-done
-
-RENKU_WORKING_DIR="${RENKU_WORKING_DIR:-${RENKU_MOUNT_DIR}}"
-
-RENKU_BASE_URL_PATH="${RENKU_BASE_URL_PATH:-/}"
-if [[ "${RENKU_BASE_URL_PATH}" != */ ]]; then
-  RENKU_BASE_URL_PATH="${RENKU_BASE_URL_PATH}/"
-fi
-
-RENKU_SESSION_IP="${RENKU_SESSION_IP:-0.0.0.0}"
-RENKU_SESSION_PORT="${RENKU_SESSION_PORT:-8888}"
-
+MOUNTDIR=${__MOUNTDIR__}
+mkdir -p "${MOUNTDIR}/.vscode/extensions"
 /codium-server/bin/codium-server \
-  --server-base-path "${RENKU_BASE_URL_PATH}" \
-  --host "${RENKU_SESSION_IP}" \
-  --port "${RENKU_SESSION_PORT}" \
-  --extensions-dir "${RENKU_MOUNT_DIR}/.vscode/extensions" \
-  --server-data-dir "${RENKU_MOUNT_DIR}/.vscode" \
-  --without-connection-token \
-  --accept-server-license-terms \
-  --telemetry-level off \
-  --default-folder "${RENKU_WORKING_DIR}"
+	--server-base-path "$RENKU_BASE_URL_PATH/" \
+	--without-connection-token \
+	--host 0.0.0.0 \
+	--port 8888 \
+	--extensions-dir "${MOUNTDIR}/.vscode/extensions" \
+	--accept-server-license-terms \
+	--telemetry-level off \
+	--server-data-dir "${MOUNTDIR}/.vscode"
